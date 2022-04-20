b0d14cdccd5676a2725baaba1c0ee7cadf66d814
fellowbeard <virgovibe@gmail.com>
2022-04-19 16:11:44 -1000

    adds updates to homeview

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,11 +5,21 @@ export default {
     return {
       message: "Welcome to Vue.js!",
       posts: [],
+      articles: [],
     };
   },
   created: function () {
     this.postsIndex();
     this.newsIndex();
+    var code = this.$route.query.code;
+    console.log(code);
+    if (code) {
+      axios.get("/auth/github/callback?code=" + code).then((response) => {
+        localStorage.setItem("github_access_token", response.data.access_token);
+        console.log(response.data.access_token);
+        this.$router.push("/about");
+      });
+    }
   },
   methods: {
     postsIndex: function () {
@@ -21,6 +31,7 @@ export default {
     newsIndex: function () {
       axios.get("/news_api").then((response) => {
         console.log(response.data);
+        this.articles = response.data.articles;
       });
     },
   },
@@ -30,6 +41,7 @@ export default {
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <a href="https://github.com/login/oauth/authorize?client_id=7b0f38f873ca98fcece8">Get in to Github</a>
     <div v-for="post in posts" v-bind:key="post.id">
       <h2>{{ post.title }}</h2>
       <p>{{ post.body }}</p>
